cba979a16ea7112ab47ba984baeb7bc1beb9d3a1
Ivan Chulkov <chulkov.i@yandex.ru>
2024-11-10 19:38:20 +0300

    feat[components]: Init CardList component

---
+++ src/components/CardList/CardList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card-list">
+    <template v-for="i in 5">
+      <Card />
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Card } from 'components/_common';
+</script>
+
+<style scoped lang="scss">
+
+</style>
