741b0f223c6b8d0c78cea7f15cc22edd27a46540
Ivan Chulkov <chulkov.i@yandex.ru>
2024-11-10 19:39:09 +0300

    feat[components]: Get chars data on mount

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <main>
     <h1>Hello world!</h1>
+    <CardList />
   </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useCharacterStore } from 'stores/character';
+import CardList from 'components/CardList';
+
+const characterStore = useCharacterStore();
+
+onMounted(async () => {
+  await characterStore.getCharsData();
+});
+</script>
 
 <style lang="scss">
-@import './assets/styles/main.scss';
+@import './index.scss';
 </style>
