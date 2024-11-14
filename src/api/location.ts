fd49776f12a1c7b404df8d46b7611b3b5d5943e7
Ivan Chulkov <chulkov.i@yandex.ru>
2024-11-14 22:08:15 +0300

    feat[api]: add a request for locations

--- src/api/location.ts
+++ src/api/location.ts
@@ -1,1 +1,6 @@
 import { api } from './index';
+import type { ApiClient } from '@/types/api.types';
+
+export const locationApi: Pick<ApiClient, 'getLocations'> = {
+  getLocations: () => api.get('/location'),
+};
