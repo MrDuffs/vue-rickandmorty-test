f9e9c5c2260047fe4e6314290f4c95c6b7188f0b
Ivan Chulkov <chulkov.i@yandex.ru>
2024-11-14 22:12:18 +0300

    feat[components]: add Search & Dropdown to main layout

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,45 @@
 <template>
+  <header class="header">
+    <Search
+      v-model="searchQuery"
+      placeholder="Find character by name"
+    />
+    <DropdownSearch />
+  </header>
   <main class="main-layout">
-    <h1>Hello world!</h1>
     <CardList :charactersData="charactersData" />
   </main>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { storeToRefs } from 'pinia'
+import { onMounted, ref, watch } from 'vue';
+import { storeToRefs } from 'pinia';
 import { useCharacterStore } from 'stores/character';
+import { Search, DropdownSearch } from 'components/_common';
 import CardList from 'components/CardList';
 
 const charsStore = useCharacterStore();
-
 const { charactersData } = storeToRefs(charsStore);
 
+const searchQuery = ref('');
+
 onMounted(async () => {
-  await charsStore.getCharsData();  
+  await charsStore.getCharsData();
+});
+
+watch(searchQuery, async newSearchQuery => {
+  await charsStore.getCharsByName(newSearchQuery);
 });
 </script>
 
 <style lang="scss">
-@use './index.scss';
+.header {
+  // padding: 20px 20px 0 20px;
+  padding: 20px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
 </style>
