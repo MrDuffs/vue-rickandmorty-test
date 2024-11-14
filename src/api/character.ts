229b82e53d59c3dee3bce52c962b6dade9e14b4d
Ivan Chulkov <chulkov.i@yandex.ru>
2024-11-14 22:09:48 +0300

    fix[api]: fix type of chars api

--- src/api/character.ts
+++ src/api/character.ts
@@ -2,7 +2,7 @@ import { api } from './index';
 import type { ApiClient } from '@/types/api.types';
 import type { ApiResponse, QueryParams } from '@/types/character.types';
 
-export const characterApi: ApiClient = {
+export const characterApi: Pick<ApiClient, 'getCharacters'> = {
   getCharacters: (params?: QueryParams) =>
     api.get<ApiResponse>('/character', { params }),
 };
