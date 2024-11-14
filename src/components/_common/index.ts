fb36066b7265e53113d4c8fd88c1ad946c6d3694
Ivan Chulkov <chulkov.i@yandex.ru>
2024-11-14 22:11:33 +0300

    feat: import search & dropdown components

--- src/components/_common/index.ts
+++ src/components/_common/index.ts
@@ -1,4 +1,6 @@
 import Card from './Card';
+import Search from './Search';
+import DropdownSearch from './DropdownSearch';
 import LoadingSpinner from './LoadingSpinner';
 
-export { Card, LoadingSpinner };
+export { Card, Search, DropdownSearch, LoadingSpinner };
