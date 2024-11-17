79066c5aefbd72a9b2c2feda413d5061f479e7fa
Ivan Chulkov <chulkov.i@yandex.ru>
2024-11-17 20:25:19 +0300

    feat[components]: add infinite scroll, searching, selection for location

--- src/components/_common/DropdownSearch/DropdownSearch.vue
+++ src/components/_common/DropdownSearch/DropdownSearch.vue
@@ -5,9 +5,8 @@
       class="dropdown-search__btn"
       :class="{ 'dropdown-search__btn_expanded': isExpanded }"
     >
-      <span v-if="!isExpanded">{{ selectedOption || 'Choose location' }}</span>
       <input
-        v-else
+        v-if="isExpanded"
         v-model="searchQuery"
         @blur="handleBlur"
         @keydown.escape="collapseInput"
@@ -15,20 +14,20 @@
         ref="searchInput"
         placeholder="Search location"
       />
+      <span>{{ selectedOption || 'Choose location' }}</span>
     </div>
 
-    <ul
-      v-if="isExpanded && filteredOptions.length"
-      class="dropdown-search__list"
-    >
+    <ul v-if="isExpanded" class="dropdown-search__list" @scroll="handleScroll">
+      <!-- TODO: Реализовать норм-ый спиннер -->
+      <LoadingSpinner v-if="isLocationLoading" />
       <li
-        v-for="location in filteredOptions"
+        v-for="location in locationData"
         :key="location.id"
         class="dropdown-search__list-item"
         :class="{
           'dropdown-search__list-item_active': selectedOption === location.name,
         }"
-        @click="selectOption(location.name as string)"
+        @click="selectOption(location)"
       >
         location: {{ location.name }} <br />
         dimension: {{ location.dimension }} <br />
@@ -39,36 +38,39 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, watch, onMounted, onBeforeUnmount } from 'vue';
+import { ref, watch, onMounted, onBeforeUnmount } from 'vue';
+import { useDebounceFn } from '@vueuse/core';
 import { useLocationStore } from '@/stores/location';
+import { storeToRefs } from 'pinia';
 
-// Пример списка значений
-const options = ['Apple', 'Banana', 'Orange', 'Grapes', 'Mango', 'Pineapple'];
+import { LoadingSpinner } from 'components/_common';
+import type { Location } from '@/types/location.types';
+import { getCharsIds } from '@/utils';
+
+interface DropdownSearchEmits {
+  (e: 'get-chars-from-location', value: string[]): void;
+}
+
+const emit = defineEmits<DropdownSearchEmits>();
 
 const location = useLocationStore();
+const { locationData, isLocationLoading } = storeToRefs(location);
 
-const isExpanded = ref(false); // Управляет состоянием кнопки/поля ввода
-const searchQuery = ref(''); // Строка поиска
-const selectedOption = ref<string | null>(null); // Выбранное значение
+// Управляет состоянием кнопки/поля ввода
+const isExpanded = ref(false);
+// Строка поиска
+const searchQuery = ref('');
+// Выбранное значение
+const selectedOption = ref<string | null>(null);
 // Ссылка на элемент input для автофокусировки
 const searchInput = ref<HTMLInputElement | null>(null);
 const dropdown = ref<HTMLElement | null>(null);
 
-// Логика фильтрации на основе ввода
-const filteredOptions = computed(() => {
-  if (!searchQuery.value) return location.locationData;
-
-  const query = searchQuery.value.toLowerCase();
-  return location.locationData.filter(location =>
-    location.name.toLowerCase().includes(query)
-  );
-});
-
 const toggleInput = async () => {
-  if (!isExpanded.value && !location.locationData.length) {
+  if (!isExpanded.value && !locationData.value.length) {
     isExpanded.value = true;
-    await location.getAllLocations();
-  } else if (!isExpanded.value && location.locationData.length) {
+    await location.getLocations();
+  } else if (!isExpanded.value && locationData.value.length) {
     isExpanded.value = true;
   }
 };
@@ -77,27 +79,38 @@ const collapseInput = () => {
   isExpanded.value = false;
 };
 
-// Выбор опции из списка
-const selectOption = (option: string) => {
-  selectedOption.value = option;
+const selectOption = async (option: Location) => {
+  selectedOption.value = option.name;
+  const charIds = getCharsIds(option.residents);
+
+  emit('get-chars-from-location', charIds);
   collapseInput();
 };
 
-// Обработка клика вне компонента
 const handleClickOutside = (event: MouseEvent) => {
   if (dropdown.value && !dropdown.value.contains(event.target as Node)) {
     collapseInput();
   }
 };
 
-// Обработчик для поля ввода при потере фокуса
-const handleBlur = (event: FocusEvent) => {
+const handleBlur = () => {
   // Используем setTimeout, чтобы дать время на выбор элемента списка перед закрытием
   setTimeout(() => {
     if (!dropdown.value?.contains(document.activeElement)) {
       collapseInput();
     }
   }, 100);
+};
+
+const handleScroll = async (event: Event) => {
+  const element = event.target as HTMLElement;
+
+  const scrollPosition = element.scrollTop + element.clientHeight;
+  const scrollHeight = element.scrollHeight;
+
+  if (scrollHeight - scrollPosition <= 50 && !isLocationLoading.value) {
+    await location.loadMoreLocations();
+  }
 };
 
 // Добавляем и удаляем обработчики кликов вне компонента
@@ -115,6 +128,14 @@ watch(isExpanded, isExpandedValue => {
       searchInput.value?.focus();
     }
   }, 100);
+});
+
+const debouncedSearchFn = useDebounceFn(async (query: string) => {
+  await location.getLocationByName(query);
+}, 800);
+
+watch(searchQuery, async newSearchQuery => {
+  await debouncedSearchFn(newSearchQuery);
 });
 </script>
 
