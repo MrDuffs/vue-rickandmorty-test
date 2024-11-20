76589dc90dd5ed1e6ebb67b829f82f41b4c220b5
Ivan Chulkov <chulkov.i@yandex.ru>
2024-11-20 15:42:32 +0300

    feat[components]: removed header into separate component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <header class="header">
-    <Search v-model="searchQuery" placeholder="Find character by name" />
-    <DropdownSearch @get-chars-from-location="handleSearchCharsByLocation" />
-  </header>
+  <Header />
   <main class="main-layout">
     <!-- <CardList :charactersData="charactersData" /> -->
     <CardList />
@@ -10,39 +7,14 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue';
-import { storeToRefs } from 'pinia';
+import { onMounted } from 'vue';
 import { useCharacterStore } from 'stores/character';
-import { Search, DropdownSearch } from 'components/_common';
 import CardList from 'components/CardList';
+import Header from './components/Header';
 
 const charsStore = useCharacterStore();
-// const { charactersData } = storeToRefs(charsStore);
-
-const searchQuery = ref('');
-
-const handleSearchCharsByLocation = async (charsArr: string[]) => {
-  await charsStore.getCharsFromLocation(charsArr)
-};
 
 onMounted(async () => {
   await charsStore.getCharsData();
 });
-
-watch(searchQuery, async newSearchQuery => {
-  await charsStore.getCharsByName(newSearchQuery);
-});
 </script>
-
-<style lang="scss">
-.header {
-  padding: 20px;
-  min-height: 50px;
-
-  display: flex;
-  justify-content: center;
-  // align-items: center;
-  align-items: stretch;
-  gap: 10px;
-}
-</style>
